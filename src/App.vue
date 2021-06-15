973cb67474a55149f039740e58234425a687da59
hzy <1257664828@qq.com>
2021-06-15 17:03:07 +0800

    异步加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,110 @@
 <template>
   <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="100" height="100" />
     <h1>{{ count }}</h1>
     <h1>{{ double }}</h1>
-  </div>
-  <ul>
-    <li v-for="number in numbers" :key="number">{{ number }}</li>
-  </ul>
-  <h1>{{ person.name }}</h1>
-  <h1>x:{{ x }},y:{{ y }}</h1>
-  <h1 v-if="loading">Loading....</h1>
-  <h1>
+    <h1>{{ greetings }}</h1>
+    <p>{{ error }}</p>
+    <Suspense>
+      <template #default>
+        <div>
+          <async-show />
+          <dog-show />
+        </div>
+      </template>
+      <template #fallback>
+        <h1>Loading !...</h1>
+      </template>
+    </Suspense>
+    <button @click="openModal">Open Modal</button><br />
+    <modal :isOpen="modalIsOpen" @close-modalone="onModalClose">
+      My Modal !!!!</modal
+    >
+    <h1 v-if="loading">Loading!...</h1>
     <img v-if="loaded" :src="result[0].url" width="100" height="100" />
-  </h1>
-  <button @click="increase">👍🏻+1</button>
-  <button @click="updateGreeting">同步更新title</button>
+    <h1>X: {{ x }}, Y: {{ y }}</h1>
+    <button @click="increase">👍+1</button><br />
+    <button @click="updateGreeting">Update Title</button>
+  </div>
 </template>
 
 <script lang="ts">
-import {
-  ref,
-  computed,
-  reactive,
-  toRefs,
-  onMounted,
-  onUpdated,
-  onRenderTriggered,
-  watch,
-} from "vue";
+import { ref, computed, reactive, toRefs, watch, onErrorCaptured } from "vue";
 import useMousePosition from "./hooks/useMousePosition";
 import useURLLoader from "./hooks/useURLLoader";
+import Modal from "./components/Modal.vue";
+import AsyncShow from "./components/AsyncShow.vue";
+import DogShow from "./components/DogShow.vue";
 interface DataProps {
   count: number;
   double: number;
   increase: () => void;
-  numbers: number[];
-  person: { name?: string };
 }
-
 interface DogResult {
   message: string;
   status: string;
 }
-
 interface CatResult {
   id: string;
   url: string;
   width: number;
   height: number;
 }
-
 export default {
   name: "App",
+  components: {
+    Modal,
+    AsyncShow,
+    DogShow,
+  },
   setup() {
-    // const count = ref(0);
-    // const double = computed(() => {
-    //   return count.value * 2;
-    // });
-    // const increase = () => {
-    //   count.value++;
-    // };
-    onMounted(() => {
-      console.log("mounted");
+    const error = ref(null);
+    onErrorCaptured((e: any) => {
+      error.value = e;
+      return true;
     });
-    onUpdated(() => {
-      console.log("updated");
-    });
-    onRenderTriggered((event) => {
-      console.log(event);
-    });
+
     const data: DataProps = reactive({
       count: 0,
       increase: () => {
         data.count++;
       },
       double: computed(() => data.count * 2),
-      numbers: [1, 2, 3],
-      person: {},
     });
-
     const { x, y } = useMousePosition();
-    const { result, loading, loaded } = useURLLoader<DogResult[]>(
+    const { result, loading, loaded } = useURLLoader<CatResult[]>(
       "https://api.thecatapi.com/v1/images/search?limit=1"
     );
     watch(result, () => {
       if (result.value) {
-        console.log("value", result.value[0]);
+        console.log("value", result.value[0].url);
       }
     });
-
     const greetings = ref("");
     const updateGreeting = () => {
       greetings.value += "Hello! ";
     };
-    watch([greetings, () => data.count], (newValue, oldVlue) => {
-      console.log("old", oldVlue);
-      console.log("new", newValue);
-      document.title = "updated" + greetings.value + data.count;
-    });
     const refData = toRefs(data);
+    const modalIsOpen = ref(false);
+    const openModal = () => {
+      modalIsOpen.value = true;
+    };
+    const onModalClose = () => {
+      modalIsOpen.value = false;
+    };
     return {
       ...refData,
+      greetings,
       updateGreeting,
       x,
       y,
       result,
       loading,
       loaded,
+      modalIsOpen,
+      openModal,
+      onModalClose,
+      error,
     };
   },
 };
---
+++ src/components/DogShow.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="result && result.message" width="100" height="100" />
+</template>
+
+<script>
+import axios from "axios";
+import { defineComponent } from "vue";
+export default defineComponent({
+  async setup() {
+    const rawData = await axios.get("https://dog.ceo/api/breeds/image/random");
+    return {
+      result: rawData.data,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
