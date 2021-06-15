1eca959efd8c535cc448887f47d6c1cb2b8f8e51
hzy <1257664828@qq.com>
2021-06-15 14:01:03 +0800

    生命周期

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,31 @@
     <h1>{{ count }}</h1>
     <h1>{{ double }}</h1>
   </div>
+  <ul>
+    <li v-for="number in numbers" :key="number">{{ number }}</li>
+  </ul>
+  <h1>{{ person.name }}</h1>
   <button @click="increase">👍🏻+1</button>
 </template>
 
 <script lang="ts">
-import { ref, computed, reactive, toRefs } from "vue";
+import {
+  ref,
+  computed,
+  reactive,
+  toRefs,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onRenderTracked,
+  onRenderTriggered,
+} from "vue";
 interface DataProps {
   count: number;
   double: number;
   increase: () => void;
+  numbers: number[];
+  person: { name?: string };
 }
 export default {
   name: "App",
@@ -23,13 +39,26 @@ export default {
     // const increase = () => {
     //   count.value++;
     // };
+    onMounted(() => {
+      console.log("mounted");
+    });
+    onUpdated(() => {
+      console.log("updated");
+    });
+    onRenderTriggered((event) => {
+      console.log(event);
+    });
     const data: DataProps = reactive({
       count: 0,
       increase: () => {
         data.count++;
       },
       double: computed(() => data.count * 2),
+      numbers: [1, 2, 3],
+      person: {},
     });
+    data.numbers[0] = 10;
+    data.person.name = "hzy";
     const refData = toRefs(data);
     return {
       ...refData,
